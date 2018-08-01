ecfc83ac8f35248067f58eda9af46ff59d6a0f1f
DeWayne <dewayne@cloudify.co>
2018-08-01 18:25:40 +0000

    example working

--- java_adapter/java.py
+++ java_adapter/java.py
@@ -2,40 +2,29 @@ from cloudify.decorators import operation
 from cloudify.exceptions import NonRecoverableError
 from cloudify import ctx
 from cloudify.proxy.server import HTTPCtxProxy
-from functools import wraps
 import os
 import json
 import subprocess
 import tempfile
 
 
-def init(jar, **kwargs):
-  """ download jars as need and put in classpath
-  """
-  dirname = mkdtemp(prefix = "javaplugin")
-  classpath = os.environ['CLASSPATH']
-  for jar in jars:
-    path = dirname+"/"+os.path.basename(jar)
-    ctx.download_resource(jar, path)
-    classpath += ":" + path
-  ctx.instance.runtime_properties['classpath'] = classpath
+
 
 ##
 ## Plugin operations
 ##
-def calljava( func, args , **kwargs):
+def calljava( opclass, args , **kwargs):
   """ Should do whatever "create" is defined as in java code.  It is assumed
-      that the java implementation main function accepts a funcion name and
+      that the java implementation main function accepts an Operation class name and
       a varargs list
   """
   proxy_server = HTTPCtxProxy(ctx._get_current_object())
   
   try:
-    jarpath = ctx.instance.runtime_properties['plugin_path']
+    jarpath = os.path.dirname(os.path.abspath(__file__))+"/plugin.jar"
     # below will fail on windows
-    os.environ['CLASSPATH'] = ctx.instance.runtime_properties['classpath']
-    res = subprocess.call([ "java" , "-jar", jarpath, str(proxy_server.port), func, json.dumps( args )])
+    res = subprocess.call([ "java" , "-jar", jarpath, str(proxy_server.port), opclass ]+args)
     if res !=0 :
-      raise NonRecoverableError("func {} execution faild".format(func))
+      raise NonRecoverableError("operation {} execution failed".format(opclass))
   finally:
     proxy_server.close()
--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 import setuptools
 import os
 
-files = None
-for _,_,files in os.walk('golang_adapter/jars'):
-  files = files
-
 setuptools.setup(
-    name='golang-test-plugin',
+    name='java-test-plugin',
     version='0.0.1',
     author='dfilppi',
     author_email='dfilppi@gmail.com',
     description='example java plugin for cloudify',
     packages=['java_adapter'],
-    package_data = {'java_adapter': [ 'jars/'+ f for f in files ] },
+    package_data = {'java_adapter': [ "plugin.jar" ] },
     install_requires = [
       'cloudify-plugins-common'
     ],
