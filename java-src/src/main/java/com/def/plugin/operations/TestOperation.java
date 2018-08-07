706bdf4b11bd45edbf3c7a36f385bacc9e4f8766
DeWayne <dewayne@cloudify.co>
2018-08-07 08:59:29 +0000

    added json decode of response

--- java-src/src/main/java/com/def/plugin/operations/TestOperation.java
+++ java-src/src/main/java/com/def/plugin/operations/TestOperation.java
@@ -1,16 +1,27 @@
 package com.def.plugin.operations;
 
-import com.def.plugin.Context;
+import com.def.plugin.context.Context;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TestOperation implements Operation {
+	
+	public static void main(String[] args) {
+		
+	}
 
 	public void execute(int ctx_port, String[] args) throws Exception{
 		Context ctx = new Context(ctx_port);
 
-		ctx.call_context("logger","info","HELLO WORLD");
 		ctx.call_context("instance","runtime_properties","some_prop","some_val");
-		String resp = ctx.call_context("instance", "runtime_properties", "some_prop", "some_val");
-		ctx.call_context("logger","info","GOT RESP:", resp);
+		ctx.call_context("logger","info","set some_prop = some_val");
+		String resp = ctx.call_context("instance", "runtime_properties", "some_prop");
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode root = mapper.readTree(resp);
+		String val = root.findValue("payload").asText();
+		ctx.call_context("logger","info","fetched some_prop = "+ val);
+				
 	}
+	
 
 }
